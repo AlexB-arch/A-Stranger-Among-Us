358c50be58c4f4536f7151a1cd2659d595e081eb
Alexander Burgos <axb19c@acu.edu>
2024-10-15 09:42:36 -0500

    Adds the NPC class.

---
+++ src/main/java/text_adventure/objects/NPC.java
@@ -0,0 +1,39 @@
+package text_adventure.objects;
+
+public class NPC {
+    private String name;
+    private String dialogue;
+    private Room location;
+
+    // Basic constructor
+    public NPC() {
+        
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDialogue(String dialogue) {
+        this.dialogue = dialogue;
+    }
+
+    public void setLocation(Room location) {
+        this.location = location;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getDialogue() {
+        return dialogue;
+    }
+
+    public Room getLocation() {
+        return location;
+    }
+    
+}
