a9d1b90c18c490ca021eae99a11a1be623c75a7f
Austin <07fi123@gmail.com>
2024-11-29 12:02:00 -0600

    graphviz inital

---
+++ src/main/java/text_adventure/objects/Elevator.java
@@ -0,0 +1,14 @@
+package text_adventure.objects;
+
+
+/**
+ * Elevator class that transports the player between decks 
+ */
+public class Elevator {
+    private String name, description;
+    private Room Deck1, Deck2, Deck3;
+    
+    public Elevator(String name, String description){
+        
+    }
+}
---
+++ src/main/java/text_adventure/objects/RoomVisualizer.java
@@ -0,0 +1,57 @@
+package text_adventure.objects;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * Room visualizer that takes rooms from our world and builds a graphic visualization.
+ * We will use this to build our visualization for our rooms. 
+ * Currently it only supports the standard type of room. Will add support to add Elevators 
+ * when elevators are in.
+ */
+public class RoomVisualizer implements AutoCloseable{
+    private StringBuilder dot;
+    private FileWriter writer;
+
+    public RoomVisualizer() {
+        dot = new StringBuilder();
+        dot.append("digraph GameMap {\n");
+        dot.append("    node [shape=box, style=filled, fillcolor=lightgray];\n");
+        dot.append("    edge [dir=both];\n");
+    }
+    
+    public void addRoom(Room room) {
+        String roomId = room.getName().replaceAll("\\s+", "_");
+        dot.append(String.format("    %s [label=\"%s\"];\n", 
+                   roomId, room.getName()));
+        
+        addConnection(room, room.getNorth(), "north");
+        addConnection(room, room.getSouth(), "south");
+        addConnection(room, room.getEast(), "east");
+        addConnection(room, room.getWest(), "west");
+    }
+    
+    private void addConnection(Room from, Room to, String direction) {
+        if (to != null) {
+            String fromId = from.getName().replaceAll("\\s+", "_");
+            String toId = to.getName().replaceAll("\\s+", "_");
+            dot.append(String.format("    %s -> %s [label=\"%s\"];\n", 
+                       fromId, toId, direction));
+        }
+    }
+    
+    public void saveToFile(String filename) throws IOException {
+        dot.append("}");
+        writer = new FileWriter(filename);
+        writer.write(dot.toString());
+        
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (writer != null){
+            writer.close();
+        }
+    }
+}
