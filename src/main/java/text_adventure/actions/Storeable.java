7fbef480527070ad8be3a45c4c125f9b250674e3
Koopa103 <89483450+Koopa103@users.noreply.github.com>
2024-12-03 15:29:20 -0600

    Add documentation for Storeable interface and its purpose

--- src/main/java/text_adventure/actions/Storeable.java
+++ src/main/java/text_adventure/actions/Storeable.java
@@ -1,5 +1,10 @@
 package text_adventure.actions;
 
+/**
+ * Storeable - Alex
+ * Interface for objects that can be stored and retrieved.
+ */
+
 public interface Storeable {
     void store();
     void retrieve();
