cdd586df12a236f0ae20338173d02c11e0bd3427
Koopa103 <89483450+Koopa103@users.noreply.github.com>
2024-12-03 15:52:52 -0600

    Update documentation for Game class to reflect multiple contributors

--- src/main/java/text_adventure/Game.java
+++ src/main/java/text_adventure/Game.java
@@ -15,7 +15,7 @@ import text_adventure.objects.triggers.AliceDialog;
 import text_adventure.objects.triggers.EndGame;
 
 /**
- * Game - Austin
+ * Game - Various people
  * The Game class is the main class that runs the game. It initializes the player, the world, and the console manager.
  * 
  * The game class is responsible for starting the game, showing the intro message, and running the game loop.
