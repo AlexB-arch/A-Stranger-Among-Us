419e11fe45048657c3b4e23994e762d7e3c30c47
Koopa103 <89483450+Koopa103@users.noreply.github.com>
2024-12-03 15:30:27 -0600

    Add documentation for Trigger interface and its purpose

--- src/main/java/text_adventure/interfaces/Trigger.java
+++ src/main/java/text_adventure/interfaces/Trigger.java
@@ -1,5 +1,10 @@
 package text_adventure.interfaces;
 
+/**
+ * Trigger - Brendan
+ * Interface for objects that can be triggered.
+ */
+
 public interface Trigger {
     boolean Condition();
     void activate();
