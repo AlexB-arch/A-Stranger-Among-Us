48a13f80515acbadf5cff27b2574e60c784d1fef
Koopa103 <89483450+Koopa103@users.noreply.github.com>
2024-12-03 15:40:53 -0600

    Add documentation for Message interface and its purpose

--- src/main/java/text_adventure/objects/Message.java
+++ src/main/java/text_adventure/objects/Message.java
@@ -1,5 +1,11 @@
 package text_adventure.objects;
 
+/**
+ * Message - Austin
+ * Interface for the Message Bus systems messages.
+ * 
+ */
+
 public interface Message {
     String getHeader();
     String getType();
